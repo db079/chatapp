021d3e5a5a3e88daf29dd250aafe77cd48a91756
Daya Borkar <dayaborkar@gmail.com>
2023-12-21 19:11:45 +0530

    part 2

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -61,6 +61,7 @@ const DashboardLayout = () => {
 
   return (
     <>
+    <Stack direction="row">
       <Box p={2} sx={{ backgroundColor: theme.palette.background.paper, boxShadow: "0px 0px 2px rgba(0,0,0,0.25)", height: "100vh", width: 100 }}>
         <Stack direction="column" alignItems="center" justifyContent="space-between" sx={{ height: "100%" }} spacing={3}>
           <Stack spacing={4} alignItems={"center"}>
@@ -107,6 +108,7 @@ const DashboardLayout = () => {
         </Stack>
       </Box>
       <Outlet />
+      </Stack>
     </>
   );
 };
